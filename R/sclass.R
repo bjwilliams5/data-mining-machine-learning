8f77a07f7604bff93cb091b5ef0dede8de7008eb
bjwilliams5 <98039212+bjwilliams5@users.noreply.github.com>
2022-02-08 15:43:09 -0600

    created sclass

--- R/sclass.R
+++ R/sclass.R
@@ -7,5 +7,78 @@ library(parallel)
 library(foreach)
 library(here)
 
-
 sclass <- read_csv(here("Data/sclass.csv"))
+
+
+## Let's take a look at the data
+
+sclass_clean <- sclass %>% 
+  select(trim, mileage, price) %>% 
+  filter(trim == "350" | trim == "65 AMG")
+
+ggplot(data = sclass_clean) + 
+  geom_point(mapping = aes(x = mileage, y = price)) +
+  facet_wrap(~ trim)
+
+## Create our datasets and compare to benchmarks
+
+sclass_350<- sclass %>% 
+  select(trim, mileage, price) %>% 
+  filter(trim == "350")
+
+lm350_1 = lm(price ~ mileage, data=sclass_350)
+lm350_2 = lm(price ~ poly(mileage, 2), data=sclass_350)
+rmse(lm350_1, sclass_350)
+rmse(lm350_2, sclass_350)
+
+
+sclass_65 <- sclass %>% 
+  select(trim, mileage, price) %>% 
+  filter(trim == "65 AMG") 
+
+lm65_1 = lm(price ~ mileage, data=sclass_65)
+lm65_2 = lm(price ~ poly(mileage, 2), data=sclass_65)
+rmse(lm65_1, sclass_65)
+rmse(lm65_2, sclass_65)
+
+## Create train / test splits
+
+sclass_350_split =  initial_split(sclass_350, prop=0.9)
+sclass_350_train = training(sclass_350_split)
+sclass_350_test  = testing(sclass_350_split)
+
+sclass_65_split =  initial_split(sclass_65, prop=0.9)
+sclass_65_train = training(sclass_65_split)
+sclass_65_test  = testing(sclass_65_split)
+
+## Define a series of k values
+
+k_grid = c(2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 25, 30, 35, 40, 45,
+           50, 60, 70, 80, 90, 100)
+
+## Run a loop to determine the RMSE for each value of k.
+
+## For 350 Trim
+
+knn_350 <- foreach(k = k_grid, .combine='rbind') %do% { 
+  rmse(knnreg(price ~ mileage, data=sclass_350_train, k=k), sclass_350_test)
+  
+}
+
+knn350_rmse <- data.frame(knn_350, k_grid)
+
+ggplot(knn350_rmse) +
+  geom_line(aes(x = k_grid, y = knn_350))
+
+
+## For 350 Trim
+
+knn_65 <- foreach(k = k_grid, .combine='rbind') %do% { 
+  rmse(knnreg(price ~ mileage, data=sclass_65_train, k=k), sclass_65_test)
+  
+}
+
+knn65_rmse <- data.frame(knn_65, k_grid)
+
+ggplot(knn65_rmse) +
+  geom_line(aes(x = k_grid, y = knn_65))
