62ab27a9b4c71cf2966b6e9132eeb9c47b5e1577
bjwilliams5 <98039212+bjwilliams5@users.noreply.github.com>
2022-02-08 10:44:25 -0600

    billboard complete

--- R/billboard.R
+++ R/billboard.R
@@ -22,11 +22,19 @@ ggplot(billboard_byyear) +
   
 
 billboard_10week <- billboard %>% 
-  filter(year > 1958, year <2021) %>% 
-  filter(week >= 10) %>% 
-  select(performer, song) %>% 
-  unique() %>% 
+  select(song, performer, week) %>% 
+  group_by(song, performer) %>% 
+  summarize(count = n()) %>% 
+  filter(count >= 10) %>% 
   group_by(performer) %>% 
   summarize(count = n()) %>% 
   filter(count >= 30)
-  
+
+billboard_10week %>% 
+  ggplot(aes(count, fct_reorder(performer, count))) + 
+  geom_col() +
+  labs(title="Billboard Top Performers", 
+       subtitle="Number of Songs on Billboard for at least 10 Weeks",
+       x="Number of Songs",
+       y="Artist") +
+  theme(plot.title.position = 'plot')
